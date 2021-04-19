b2593ed70bc554c571856d5de565bc1a8c4c48c1
viduni94 <viduni.ushanka@gmail.com>
2021-04-19 09:53:36 +0530

    Added login form UI

---
+++ PersonalFinanceApp/LoginForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace PersonalFinanceApp
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.inputUsername = new System.Windows.Forms.TextBox();
+            this.inputPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.loginImage = new System.Windows.Forms.PictureBox();
+            this.lblExitLink = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.loginImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputUsername
+            // 
+            this.inputUsername.Location = new System.Drawing.Point(470, 422);
+            this.inputUsername.Name = "inputUsername";
+            this.inputUsername.Size = new System.Drawing.Size(456, 39);
+            this.inputUsername.TabIndex = 0;
+            // 
+            // inputPassword
+            // 
+            this.inputPassword.Location = new System.Drawing.Point(470, 543);
+            this.inputPassword.Name = "inputPassword";
+            this.inputPassword.Size = new System.Drawing.Size(456, 39);
+            this.inputPassword.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnLogin.Location = new System.Drawing.Point(587, 655);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(206, 76);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblUsername.Location = new System.Drawing.Point(470, 374);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(163, 45);
+            this.lblUsername.TabIndex = 4;
+            this.lblUsername.Text = "Username";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPassword.Location = new System.Drawing.Point(470, 495);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(153, 45);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Password";
+            this.lblPassword.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // loginImage
+            // 
+            this.loginImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.loginImage.Image = ((System.Drawing.Image)(resources.GetObject("loginImage.Image")));
+            this.loginImage.Location = new System.Drawing.Point(606, 124);
+            this.loginImage.Name = "loginImage";
+            this.loginImage.Size = new System.Drawing.Size(187, 187);
+            this.loginImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.loginImage.TabIndex = 6;
+            this.loginImage.TabStop = false;
+            this.loginImage.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblExitLink
+            // 
+            this.lblExitLink.AutoSize = true;
+            this.lblExitLink.Location = new System.Drawing.Point(22, 903);
+            this.lblExitLink.Name = "lblExitLink";
+            this.lblExitLink.Size = new System.Drawing.Size(178, 32);
+            this.lblExitLink.TabIndex = 7;
+            this.lblExitLink.TabStop = true;
+            this.lblExitLink.Text = "Exit Application";
+            this.lblExitLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1372, 954);
+            this.Controls.Add(this.lblExitLink);
+            this.Controls.Add(this.loginImage);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.inputPassword);
+            this.Controls.Add(this.inputUsername);
+            this.Name = "LoginForm";
+            this.Text = "Login Form";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.loginImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputUsername;
+        private System.Windows.Forms.TextBox inputPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.PictureBox loginImage;
+        private System.Windows.Forms.LinkLabel lblExitLink;
+    }
+}
+
---
+++ PersonalFinanceApp/LoginForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonalFinanceApp
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Dashboard myDashboard = new Dashboard();
+            myDashboard.Show();
+        }
+    }
+}
